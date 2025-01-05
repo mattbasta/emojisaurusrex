7177fbc54e66389241586947d92ccc1bb22b354f
Matt Basta <mattbasta@hey.com>
2025-01-05 16:37:44 -0500

    manifest v3

--- popup.js
+++ popup.js
@@ -1,17 +1,7 @@
-function copyToClipboard(text) {
-    var copyDiv = document.createElement('div');
-    copyDiv.contentEditable = true;
-    document.body.appendChild(copyDiv);
-
-    copyDiv.innerHTML = text;
-    copyDiv.unselectable = "off";
-    copyDiv.focus();
-
-    document.execCommand('SelectAll');
-    document.execCommand("Copy", false, null);
-    document.body.removeChild(copyDiv);
-}
-
-document.body.addEventListener('click', function(e) {
-	copyToClipboard(e.target.innerHTML);
-}, true);
+document.body.addEventListener(
+  "click",
+  function (e) {
+    navigator.clipboard.writeText(e.target.innerHTML);
+  },
+  true
+);
